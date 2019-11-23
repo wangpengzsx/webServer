fb3f9cb0a41201c922883abb79a3e5d78b623a31
wangpeng43 <wangpeng43@lenovo.com>
2019-11-23 19:59:02 +0800

    第一次创建

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+//引入插件
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+
+//使用connect启动一个Web服务器
+gulp.task('connect', function () {
+    connect.server({
+        root: 'app',
+        port:process.env.PORT || 8088
+    });
+});
+
+//运行Gulp时，默认的Task
+gulp.task('default', ['connect']);
